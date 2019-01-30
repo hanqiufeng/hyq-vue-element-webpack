5568a77e48a1813b9ca0c4bc1f3191f846d8310e
hyq <1713877106@qq.com>
2019-01-30 10:22:30 +0800

    123

--- src/views/documentation/index.vue
+++ src/views/documentation/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="app-container documentation-container">
-    <a class="document-btn" target='_blank' href="https://panjiachen.github.io/vue-element-admin-site/">{{$t('documentation.documentation')}}</a>
+    <a class="document-btn" target='_blank' href="https://panjiachen.github.io/vue-element-admin-site/zh/">{{$t('documentation.documentation')}}</a>
     <a class="document-btn" target='_blank' href="https://github.com/PanJiaChen/vue-element-admin/">{{$t('documentation.github')}}</a>
     <dropdown-menu style="float:left;margin-left:50px;" title='系列文章' :items='articleList'></dropdown-menu>
   </div>
@@ -14,14 +14,38 @@ export default {
   data() {
     return {
       articleList: [
-        { title: '基础篇', href: 'https://juejin.im/post/59097cd7a22b9d0065fb61d2' },
-        { title: '登录权限篇', href: 'https://juejin.im/post/591aa14f570c35006961acac' },
-        { title: '实战篇', href: 'https://juejin.im/post/593121aa0ce4630057f70d35' },
-        { title: 'vueAdmin-template 篇', href: 'https://juejin.im/post/595b4d776fb9a06bbe7dba56' },
-        { title: '自行封装 component', href: 'https://segmentfault.com/a/1190000009090836' },
-        { title: '优雅的使用 icon', href: 'https://juejin.im/post/59bb864b5188257e7a427c09' },
-        { title: 'webpack4（上）', href: 'https://juejin.im/post/59bb864b5188257e7a427c09' },
-        { title: 'webpack4（下）', href: 'https://juejin.im/post/5b5d6d6f6fb9a04fea58aabc' }
+        {
+          title: '基础篇',
+          href: 'https://juejin.im/post/59097cd7a22b9d0065fb61d2'
+        },
+        {
+          title: '登录权限篇',
+          href: 'https://juejin.im/post/591aa14f570c35006961acac'
+        },
+        {
+          title: '实战篇',
+          href: 'https://juejin.im/post/593121aa0ce4630057f70d35'
+        },
+        {
+          title: 'vueAdmin-template 篇',
+          href: 'https://juejin.im/post/595b4d776fb9a06bbe7dba56'
+        },
+        {
+          title: '自行封装 component',
+          href: 'https://segmentfault.com/a/1190000009090836'
+        },
+        {
+          title: '优雅的使用 icon',
+          href: 'https://juejin.im/post/59bb864b5188257e7a427c09'
+        },
+        {
+          title: 'webpack4（上）',
+          href: 'https://juejin.im/post/59bb864b5188257e7a427c09'
+        },
+        {
+          title: 'webpack4（下）',
+          href: 'https://juejin.im/post/5b5d6d6f6fb9a04fea58aabc'
+        }
       ]
     }
   }
